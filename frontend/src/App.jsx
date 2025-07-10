1fd14fb23fb64e9776f5ff73d75fe0c2275302fc
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-07-10 05:20:14 +0000

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './Assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from "./pages/Home.jsx"
 
 function App() {
 
 
   return (
-    <div>
-       <Routes>
-        <Route path="/" element={<Home/>}/>
-       </Routes>
+    <div className="min-h-screen w-full bg-richblack-900 flex flex-col font-inter overflow-x-hidden overflow-y-auto">
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
