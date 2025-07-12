05ca8c8be4f6402eba00ce9c2d87b15dc5190f0c
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-07-12 16:49:24 +0000

    Add background gradients and styling to code blocks; update Home component layout and links

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,31 +11,32 @@ import InstructorSection from '../components/core/HomePage/InstructorSection.jsx
 import ReviewSlider from '../components/core/HomePage/ReviewSlider.jsx';
 import Footer from '../components/common/Footer';
 import ExploreMore from '../components/core/HomePage/ExploreMore.jsx';
+
+
 const Home = () => {
     return (
         <div>
 
             {/* Section 1 */}
-            <div className='relative mx-auto flex flex-col w-11/12 items-center text-white justify-between'>
+            <div className='relative mx-auto flex flex-col w-11/12 items-center text-white justify-between gap-8 text-white'>
 
 
                 <Link to="/signup">
 
-                    <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit'>
-                        <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
-                        transition-all duration-200 group-hover:bg-richblack-900'>
+                    <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
+                        <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
                             <p>Become an Instructor</p>
                             <FaArrowRight />
                         </div>
                     </div>
                 </Link>
 
-                <div className='text-center text-4xl font-semibold mt-7'>
+                <div className='text-center text-4xl font-semibold '>
                     Empower Your Future with
                     <HighlightText text={"Coding Skills"} />
                 </div>
 
-                <div className='mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
+                <div className='-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300'>
                     With our online coding courses, you can learn at your own pace, from
                     anywhere in the world, and get access to a wealth of resources,
                     including hands-on projects, quizzes, and personalized feedback from
@@ -53,8 +54,10 @@ const Home = () => {
                     </CTAButton>
                 </div>
 
-                <div className='mx-3 my-12 shadow-blue-200'>
+                {/* Video */}
+                <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
                     <video
+                        className='shadow-[20px_20px_rgba(255,255,255)]'
                         muted
                         loop
                         autoPlay
@@ -88,14 +91,14 @@ const Home = () => {
                             {
                                 btnText: "learn more",
                                 active: false,
-                                linkto: "/login",
+                                linkto: "/signup",
                             }
                         }
 
                         codeblock={`<!DOCTYPE html>
                         <html>\n<head>\n<title>Example</title>\n<linkrel="stylesheet"\nhref="styles.css">\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a></nav>`}
                         codeColor={"text-yellow-25"}
-                        backgroundGradient={""}
+                        backgroundGradient={<div className='codeblock1 absolute'></div>}
 
                     />
                 </div>
@@ -105,39 +108,38 @@ const Home = () => {
                     <CodeBlocks
                         position={"lg:flex-row-reverse"}
                         heading={
-                            <div className='text-4xl font-semibold'>
-                                Unlock Your
-                                <HighlightText text={"Coding Potential"} />
-                                with our online courses
+                            <div className='w-[100%] lg:w-[50%] text-4xl font-semibold'>
+                                Start
+                                <HighlightText text={"Coding in seconds"} />
+
                             </div>
                         }
                         subheading={
-                            "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+                            "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
                         }
                         ctabtn1={
                             {
-                                btnText: "Try it Yourself",
+                                btnText: "Continue Lesson",
                                 active: true,
                                 linkto: "/signup",
                             }
                         }
                         ctabtn2={
                             {
-                                btnText: "learn more",
+                                btnText: "Learn more",
                                 active: false,
-                                linkto: "/login",
+                                linkto: "/signup",
                             }
                         }
 
-                        codeblock={`<!DOCTYPE html>
-                        <html>\n<head>\n<title>Example</title>\n<linkrel="stylesheet"\nhref="styles.css">\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a></nav>`}
-                        codeColor={"text-yellow-25"}
-                        backgroundGradient={""}
+                        codeColor={"text-white"}
+                        codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+                        backgroundGradient={<div className="codeblock2 absolute"></div>}
 
                     />
                 </div>
 
-                <ExploreMore/>
+                <ExploreMore />
             </div>
 
             {/* Section 2 */}
@@ -210,7 +212,7 @@ const Home = () => {
             </div>
 
             {/* footer */}
-           <Footer/>
+            <Footer />
 
         </div>
     )
