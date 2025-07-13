2f2eee67caa772276910a5662178978e399881a6
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-07-13 03:06:50 +0000

    Refactor ExploreMore component for improved readability and consistency; update formatting and spacing

--- frontend/src/components/core/HomePage/ExploreMore.jsx
+++ frontend/src/components/core/HomePage/ExploreMore.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HomePageExplore} from '../../../data/homepage-explore.js';
+import { HomePageExplore } from '../../../data/homepage-explore.js';
 import { useState } from 'react';
 import HighlightText from './HighlightText';
 import CourseCard from './CourseCard.jsx';
@@ -16,53 +16,55 @@ const ExploreMore = () => {
 
   const [currentTab, setCurrentTab] = useState(tabsName[0]);
   const [courses, setCourses] = useState(HomePageExplore[0].courses);
-  const [currentCard,setCurrentCard] = useState(HomePageExplore[0].courses[0].heading)
+  const [currentCard, setCurrentCard] = useState(HomePageExplore[0].courses[0].heading)
 
-  const setMyCards = (value) =>{
+  const setMyCards = (value) => {
     setCurrentTab(value);
-    const result = HomePageExplore.filter((courses)=>courses.tag===value);
+    const result = HomePageExplore.filter((courses) => courses.tag === value);
     setCourses(result[0].courses);
     setCurrentCard(result[0].courses[0].heading);
   }
 
   return (
     <div>
-      <div className='text-4xl font-semibold text-center'>
-        Unlock the 
-        <HighlightText text={"Power of Code"} />
+      {/* Explore more section */}
+      <div>
+        <div className="text-4xl font-semibold text-center my-10">
+          <div className="container" style={{ display: "block" }}>
+            Unlock the
+            <HighlightText style={{ display: "inline" }} text={"Power of Code"} />
+          </div>
+          <p className="text-center text-richblack-300 text-sm font-semibold mt-1">
+            Learn to Build Anything You Can Imagine
+          </p>
+        </div>
+      </div>
+      {/* Tabs Section */}
+      {/* Tabs Section */}
+      <div className="hidden lg:flex gap-5 -mt-5 mx-auto w-max bg-richblack-800 text-richblack-200 p-1 rounded-full font-medium drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]">
+        {tabsName.map((ele, index) => {
+          return (
+            <div
+              className={`text-[16px] flex flex-row items-center gap-2 ${currentTab === ele
+                ? "bg-richblack-900 text-richblack-5 font-medium"
+                : "text-richblack-200"
+                } px-7 py-[7px] rounded-full transition-all duration-200 cursor-pointer hover:bg-white hover:text-richblack-900 hover:drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]`}
+              key={index}
+              onClick={() => setMyCards(ele)}
+            >
+              {ele}
+            </div>
+          );
+        })}
       </div>
 
-      <p className='text-center text-richblack-300 text-[16px] mt-3 '>
-        Learn to build Anything you can Imagine.
-      </p>
-
-      <div className='flex flex-row rounded-full bg-richblack-800 mb-5 mt-5 border-richblack-100 px-1 py-1'>
-        {
-            tabsName.map((element,index)=>{
-                return (
-                    <div
-                    className={`text-[16px] flex flex-row items-center gap-2
-                      ${currentTab===element ? "bg-richblack-900 text-richblack-5 font-medium"
-                        :"text-richblack-200"
-                      } rounded-full transition-all duration-200 cursor-pointer
-                      hover:bg-richblack-900 hover:text-richblack-5 px-7 py-7`}
-                      key={index}
-                      onClick={() => setMyCards(element)}
-                    >
-                        {element}
-                    </div>
-                )
-            })
-        }
-      </div>
-
-      <div className='lg:h-[150px]'> </div>
+      <div className='hidden lg:block lg:h-[200px]'> </div>
 
       {/* Course Card Ka Group */}
 
       <div className='lg:absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%] text-black lg:mb-0 mb-7 lg:px-0 px-3'>
         {
-          courses.map((element,index)=>{
+          courses.map((element, index) => {
             return (
               <CourseCard
                 key={index}
