b3f8afb99b14dbb02a0e550ac21bfe60fb61c866
Rishabh Kumar <rishabhgzp2004@gmail.com>
2025-08-17 19:34:34 +0530

    feat: Import ReviewSlider component in About and Contact pages; enable display of learner reviews

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,7 +8,7 @@ import LearningGrid from "../components/core/AboutPage/LearningGrid"
 import Quote from "../components/core/AboutPage/Quote"
 import StatsComponenet from "../components/core/AboutPage/Stats"
 import HighlightText from "../components/core/HomePage/HighlightText"
-///import ReviewSlider from "../components/common/ReviewSlider"
+import ReviewSlider from '../components/common/ReviewSlider.jsx';
 import Footer from "../components/common/Footer"
 const About = () => {
   return (
@@ -112,7 +112,7 @@ const About = () => {
           Happy & Satisfied Learners!
         </h1>
         {/* <ReviewSlider /> */}
-        {/* <ReviewSlider /> */}
+        <ReviewSlider />
       </div>
       {/* <Footer /> */}
       <Footer />
